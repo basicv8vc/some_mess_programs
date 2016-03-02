caadce034fc50625b382088d8a97cb126122c840
Li Jia <basicv8vc@gmail.com>
2016-03-02 22:17:46 +0800

    add twosum.cc
    vector<PAIR>, runtime:12ms

---
+++ leetcode/1 two sum/twosumv.cc
@@ -0,0 +1,50 @@
+//Runtime:12ms,  beats 85.58%
+//先排序，然后首尾两个指针，向内移动,但要注意要返回原始数组的下标而不是排序后的数组下标,所以要用map<index, value>并且对value排序，
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    
+    vector<PAIR> result;
+	
+	for (unsigned i = 0; i < nums.size(); ++i)
+	{
+		result.push_back(PAIR(i,nums[i]));
+		
+	}
+
+	
+	sort(result.begin(), result.end(),cmp);
+
+	unsigned i = 0, j= result.size()-1;
+	vector<int> outcome;
+	while (i < j)
+	{
+		
+		if ((result[i].value + result[j].value) == target)
+		{
+			int min = (result[i].index < result[j].index) ? result[i].index : result[j].index;//下标从小到大输出
+			int max = (result[i].index < result[j].index) ? result[j].index : result[i].index;
+			outcome.push_back(min);
+			outcome.push_back(max);
+			return outcome;
+		}
+		else if ((result[i].value + result[j].value) < target)
+			++i;
+		else {
+			--j;
+		
+		}
+		
+	}
+        
+    }
+    struct PAIR {
+	    int index;
+	    int value;
+	    PAIR(int a, int b) :index(a), value(b) {}
+    };
+    static bool cmp(PAIR a, PAIR b) //
+    {
+	    return a.value < b.value;
+    }
+};
