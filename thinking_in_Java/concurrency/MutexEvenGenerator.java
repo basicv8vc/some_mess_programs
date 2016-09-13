5292903647ad8ba41ec7390bcfdafcacad892f5d
jia <basicv8vc@gmail.com>
2016-09-13 16:24:40 +0800

    add some examples

---
+++ thinking_in_Java/concurrency/MutexEvenGenerator.java
@@ -0,0 +1,27 @@
+package thinking.chapter.concurrency;
+
+import java.util.concurrent.locks.*;
+
+
+public class MutexEvenGenerator extends IntGenerator {
+	private int currentEvenValue = 0;
+	private Lock lock = new ReentrantLock();
+	public int next() {
+		lock.lock();
+		try {
+			++currentEvenValue;
+			Thread.yield();
+			++currentEvenValue;
+			return currentEvenValue;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EvenChecker.test(new MutexEvenGenerator());
+
+	}
+
+}
