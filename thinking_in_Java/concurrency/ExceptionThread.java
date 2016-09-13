06d28bee9b061a78638634a1d3325ddf73ec6262
jia <basicv8vc@gmail.com>
2016-09-13 11:10:21 +0800

    add join and exception examples

---
+++ thinking_in_Java/concurrency/ExceptionThread.java
@@ -0,0 +1,19 @@
+package thinking.chapter.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExceptionThread implements Runnable {
+	@Override
+	public void run() {
+		throw new RuntimeException();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ExecutorService exec = Executors.newCachedThreadPool();
+		exec.execute(new ExceptionThread());
+
+	}
+
+}
