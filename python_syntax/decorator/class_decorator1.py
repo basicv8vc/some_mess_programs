6cd0b4c66b09206827b5e5711c9e4d1ded688b07
jia <basicv8vc@gmail.com>
2017-04-01 10:27:42 +0800

    submit class decorator examples

---
+++ python_syntax/decorator/class_decorator1.py
@@ -0,0 +1,20 @@
+class myDecorator:
+
+    def __init__(self, f):
+        print("inside myDecorator.__init__()")
+        f()
+
+    def __call__(self):
+        print("inside myDecorator.__call__()")
+
+@myDecorator
+def aFunction():
+    print("inside aFunction()")
+
+print "Finished decorating aFunction()"
+
+aFunction()
+
+
+
+
